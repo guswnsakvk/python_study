a25beafde9548b3e7a8ae38584d21ab801423b84
guswnsakvk <guswnsakvk@naver.com>
2021-02-27 19:48:43 +0900

    표준입출력 재업로드

--- nado_coding5.py
+++ nado_coding5.py
@@ -4,3 +4,6 @@ scores = {"수학":0, "영어":50, "코딩":100}
 for subject, score in scores.items():
     #print(subject, score)
     print(subject.ljust(8), str(score).rjust(4), sep = ":")
+
+for num in range(1, 21):
+    print("대기번호 : " + str(num).zfill(3))
