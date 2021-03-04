e5469024f1884259cb252d737c027c32b80a91c7
guswnsakvk <guswnsakvk@naver.com>
2021-03-04 22:19:03 +0900

    module 재업

--- nado_coding21.py
+++ nado_coding21.py
@@ -8,13 +8,20 @@ mv.price(3)
 mv.price_morning(4)
 mv.price_soldier(5)
 
-from theater_module import *
+from theater_module import * # theater_module에 모든 내용 가져오기
 price(3)
 price_morning(4)
 price_soldier(5)
 
-# theater_module
-# 일반 가격
+from theater_module import price, price_morning # theater_module에 특정 내용만 가져오기
+price(5)
+price_morning(6)
+
+from theater_module import price_soldier as price # theater_module에 특정 내용만 가져오는데 별명 만들기
+price(5)
+
+# # theater_module
+# # 일반 가격
 def price(people):
     print("{0}명의 가격은 {1}원 입니다.".format(people, people * 10000))
 
